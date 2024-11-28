55d88b7c1c3f4e0978a561b92a55be3904bc91c3
Vinicius Ciunek <vinicius_ciunek@hotmail.com>
2024-11-28 09:05:26 -0300

    refactor: dry layout finished and added the create product page

--- public/index.php
+++ public/index.php
@@ -1,18 +1,3 @@
-<html lang="pt-br">
+<?php
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Mão na Roda</title>
-
-  <link rel="stylesheet" href="/assets/css/application.css">
-  <script src="/assets/js/application.js"></script>
-</head>
-
-<body>
-  <main>
-    <h1>Mão na Roda!</h1>
-  </main>
-</body>
-
-</html>
+header('Location: /pages/products');
