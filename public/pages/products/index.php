42dd2e1ac553c3a2a501876ea0bb6dc352ecc877
Vinicius G. Ciunek <66188738+viniciusciunek@users.noreply.github.com>
2024-11-28 11:02:34 -0300

    feature: retrive produts from txt database (#10)

--- public/pages/products/index.php
+++ public/pages/products/index.php
@@ -1,5 +1,9 @@
 <?php
 
+define("DB_PATH", '/var/www/database/products.txt');
+
+$products = file(DB_PATH, FILE_IGNORE_NEW_LINES);
+
 $title = 'Produtos Cadastrados';
 $view = '/var/www/app/views/products/index.phtml';
 
