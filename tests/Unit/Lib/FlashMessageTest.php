5261f5277091206594933bcb6c212de8ec6c550c
Vinicius G. Ciunek <66188738+viniciusciunek@users.noreply.github.com>
2024-12-01 15:11:18 -0300

    flash messages lib done (#38)

---
+++ tests/Unit/Lib/FlashMessageTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit\Lib;
+
+use Lib\FlashMessage;
+use Tests\TestCase;
+
+class FlashMessageTest extends TestCase
+{
+    public function test_success(): void
+    {
+        FlashMessage::success('Success message!');
+
+        $flash = FlashMessage::get();
+
+        $this->assertArrayHasKey('success', $flash);
+        $this->assertEquals('Success message!', $flash['success']);
+    }
+
+    public function test_danger(): void
+    {
+        FlashMessage::danger('Danger message!');
+
+        $flash = FlashMessage::get();
+
+        $this->assertArrayHasKey('danger', $flash);
+        $this->assertEquals('Danger message!', $flash['danger']);
+    }
+    public function test_get(): void
+    {
+        FlashMessage::success('Success message!');
+        FlashMessage::danger('Danger message!');
+
+        $flash = FlashMessage::get();
+
+        $this->assertEmpty(FlashMessage::get());
+
+        $this->assertArrayHasKey('success', $flash);
+        $this->assertEquals('Success message!', $flash['success']);
+
+        $this->assertArrayHasKey('danger', $flash);
+        $this->assertEquals('Danger message!', $flash['danger']);
+    }
+}
